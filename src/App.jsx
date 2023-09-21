ce577a87c19c1189f11f018af67063b35aa68822
Sven Olafsen <stockburner9@gmail.com>
2023-09-21 11:32:58 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+      <h1>Want to delete your account?</h1>
+      <div className="card">
+       
+        <p>
+         Contact fulfilhelp@gmail.com
+        </p>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
